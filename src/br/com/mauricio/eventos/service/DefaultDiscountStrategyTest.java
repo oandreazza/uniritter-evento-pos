61f2524f3c0aa989405a1ab48dfc35c8264ceccd
O Andreazza <mauricio.webdev@gmail.com>
2016-09-29 17:04:06 -0300

    TODO: implement DefaultDiscountTest

---
+++ src/br/com/mauricio/eventos/service/DefaultDiscountStrategyTest.java
@@ -0,0 +1,21 @@
+package br.com.mauricio.eventos.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Categories.ExcludeCategory;
+
+public class DefaultDiscountStrategyTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Ignore
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
